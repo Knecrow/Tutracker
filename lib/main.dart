2cdeead82a39471564fa96bbbb9d0716f605f07c
Syed Nahian <syednahian906@gmail.com>
2026-08-06 16:42:46 +0600

    fix: add services.dart import to main.dart for SystemChrome

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'core/theme/app_theme.dart';
